973df6ff55adb644b3afa0eb82265a88f23f1d4c
Kate Matthews <kmatthews.87@hotmail.co.uk>
2022-07-03 18:38:06 +0100

    update 3-7-22

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
 function topFunction() {
     window.scrollTo(0,0)
 }
-  
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+//FORM
+
+// const form  = document.getElementById('messageForm');
+
+// const name = form.elements['name'];
+// const email = form.elements['email'];
+// const subject = form.elements['subject'];
+// const message = form.elements['message'];
+
+// let fullName = name.value;
+// let emailAddress = email.value;
+// let messageSubject = subject.value;
+// let message = message.value;
+
+// form.addEventListener('submit', (event) => {
+//     // handle the form data
+// });
+
+// form.addEventListener('submit', (event) => {
+//     // stop form submission
+//     event.preventDefault();
+// });
+
+// form.submit ();
