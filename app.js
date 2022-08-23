31eaee9ad8040e6dc744255e6aac30b65779634d
Kate Matthews <kmatthews.87@hotmail.co.uk>
2022-08-23 21:58:44 +0100

    23-08-22:00

--- app.js
+++ app.js
@@ -19,3 +19,9 @@ window.onclick = function(event) {
     }
   }
 
+
+
+
+
+
+  (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='COUNTRY';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);
